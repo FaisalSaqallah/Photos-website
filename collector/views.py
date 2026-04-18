ceae361774272285b58fbd8ca73ca911d4f6ca0e
FaisalSaqallah <124791547+FaisalSaqallah@users.noreply.github.com>
2026-04-18 16:39:54 +0300

    Initial commit

---
+++ collector/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Submission
+
+def upload_view(request):
+    if request.method == 'POST':
+        height_m = request.POST.get('height_m')
+        front_photo = request.FILES.get('front_photo')
+        side_photo = request.FILES.get('side_photo')
+
+        if height_m and front_photo and side_photo:
+            Submission.objects.create(
+                height_m=height_m,
+                front_photo=front_photo,
+                side_photo=side_photo
+            )
+            messages.success(request, 'Successfully uploaded data! Thank you.')
+            return redirect('upload')
+        else:
+            messages.error(request, 'Please provide all fields.')
+            
+    return render(request, 'index.html')
